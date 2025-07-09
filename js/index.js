79ae28ca89af0eba6bb76028ed2316fc6c0e1cb3
ajimcse <mdajimsorkarcse40@gmail.com>
2025-07-09 21:50:33 +0600

    blog section and function section

--- js/index.js
+++ js/index.js
@@ -1,31 +1,76 @@
 document.getElementById('mine-click-btn').addEventListener('click', function () {
-    const inputNumber = document.getElementById('input-number').value;
-    const inputValue = parseFloat(inputNumber);
-    const addMe = document.getElementById('add-me-btn').innerText;
-    const addInerTextValue = parseFloat(addMe);
+    const inputValue = getFieldInputValue('input-number');
+    const inputText = getInputTextFiledValue('add-me-btn');
+    const mainBalance = getInputTextFiledValue('main-balance');
 
-
-    if (!isNaN(inputValue)) {
-        const addBalance = inputValue + addInerTextValue;
-        document.getElementById('add-me-btn').innerText = addBalance;
+    if (!isNaN(inputValue) && inputValue > 0 && inputValue <= mainBalance){
+        const newMainBalance = mainBalance - inputValue;
+        const newAddMeValue = inputText + inputValue;
+        document.getElementById('main-balance').innerText = newMainBalance.toFixed(2);
+        document.getElementById('add-me-btn').innerText = newAddMeValue.toFixed(2);
         document.getElementById('input-number').value = '';
-       
-       const p = document.createElement('p');
-        p.innerText = `${addInerTextValue}, Total: ${addBalance}`;
-        document.getElementById('History-section').appendChild(p);
-        
+
+        const historyContainer = document.getElementById('History-section');
+        const now = new Date();
+        const timeString = now.toLocaleTimeString();
+        const dateString = now.toLocaleDateString();
+        const historyDiv = document.createElement('div');
+        historyDiv.style.fontWeight = 'bold';
+        historyDiv.style.fontSize = '20px';
+        historyDiv.style.border = '1px solid #ccc';
+        historyDiv.style.padding = '20px';
+        historyDiv.style.margin='20px'
+        historyDiv.style.marginBottom = '20px';
+        historyDiv.style.borderRadius = '5px';
+        historyDiv.style.backgroundColor = '#f9f9f9';
+
+        historyDiv.innerText = `${inputValue} TK Donate for the Flood in Noakhali, Bangladesh,
+         Bangladesh-Date:${dateString} Time:${timeString}`;
+
+        historyContainer.appendChild(historyDiv);
+        alert('❤️Congratulations❤️, you have donated in every way.');
+
     }
     else {
-        alert('the worin number')
+        alert('Please give correct number')
     }
 
+})
+
+document.getElementById('mine-click-btn2').addEventListener('click', function () {
+    const inputValue2 = getFieldInputValue('input-number2');
+    const inputText2 = getInputTextFiledValue('add-me-btn2');
+    const mainBalance = getInputTextFiledValue('main-balance');
+
+
+    if (!isNaN(inputValue2)) {
+        const inputnNegtive = mainBalance - inputValue2;
+        const inputPrgitive = inputText2 + inputValue2;
+        document.getElementById('main-balance').innerText = inputnNegtive.toFixed(2);
+        document.getElementById('add-me-btn2').innerText = inputPrgitive.toFixed(2);
+        document.getElementById('input-number2').value = '';
+
+
+    }
+
+    else {
+        alert('Please give correct number')
+    }
 
 })
+
+
+
+
 document.getElementById('Donation-btn').addEventListener('click', function () {
     showSectionById('Donation-section')
 })
 
+
 document.getElementById('History-btn').addEventListener('click', function () {
     showSectionById('History-section')
-})
+});
+
+
+
 
--- js/utilities.js
+++ js/utilities.js
@@ -1,6 +1,20 @@
-function showSectionById(id){
+function getInputTextFiledValue(id) {
+    const inputTextvalue = document.getElementById(id).innerText;
+    const inputText = parseFloat(inputTextvalue);
+    return inputText;
+}
+
+function getFieldInputValue(id) {
+    const inputValue = document.getElementById(id).value;
+    const inputNumber = parseFloat(inputValue);
+    return inputNumber;
+}
+
+function showSectionById(id) {
     document.getElementById('Donation-section').classList.add('hidden');
     document.getElementById('History-section').classList.add('hidden');
-  
+    document.getElementById('Donation-section2').classList.add('hidden');
+    document.getElementById('Donation-section1').classList.add('hidden');
+
     document.getElementById(id).classList.remove('hidden')
 }
